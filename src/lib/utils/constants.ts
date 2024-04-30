43ef07ba750728fb853b5b7c01bcb687ba364134
Naseer Ahmed <naseer.ahmed@masterandassociatesinc.com>
2024-05-01 04:51:17 +0500

    eslint added

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,20 +1,5 @@
 export const FUNCTION = "function";
 
-export const SALE_CHANNEL_OPTIONS = [
-  "Shopify",
-  "Daraz",
-  "WooCommerce",
-  "Other",
-];
-export const SHIP_ORDER_OPTIONS = [
-  "TCS",
-  "FedEx",
-  "DHL",
-  "Blue Dart",
-];
-export const ORDERED_PER_MONTH_OPTIONS = [
-  "100-200",
-  "200-300",
-  "300-400",
-  "More then 400",
-];
+export const SALE_CHANNEL_OPTIONS = ["Shopify", "Daraz", "WooCommerce", "Other"];
+export const SHIP_ORDER_OPTIONS = ["TCS", "FedEx", "DHL", "Blue Dart"];
+export const ORDERED_PER_MONTH_OPTIONS = ["100-200", "200-300", "300-400", "More then 400"];
