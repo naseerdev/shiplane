b069dd98696566f6d5756bcf42448dd1f15cab21
Naseer Ahmed <naseer.ahmed@masterandassociatesinc.com>
2024-05-01 21:44:29 +0500

    calander link added

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -3,3 +3,4 @@ export const FUNCTION = "function";
 export const SALE_CHANNEL_OPTIONS = ["Shopify", "Daraz", "WooCommerce", "Other"];
 export const SHIP_ORDER_OPTIONS = ["TCS", "FedEx", "DHL", "Blue Dart"];
 export const ORDERED_PER_MONTH_OPTIONS = ["100-200", "200-300", "300-400", "More then 400"];
+export const CALENDLY_INVITE = 'https://calendly.com/developer-irsal/30min'
