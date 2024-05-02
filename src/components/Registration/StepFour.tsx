93472c2d9258adb9d1261841e78a16762ecae225
adeelch007 <adeelijazch@gmail.com>
2024-05-02 21:15:35 +0500

    Ui changes stepfour

--- src/components/Registration/StepFour.tsx
+++ src/components/Registration/StepFour.tsx
@@ -11,7 +11,7 @@ const StepFour: Component = ({ }) => {
     <div
       class="mx-auto mt-0 sm:mt-5 md:mt-10 max-w-[750px] sm:p-[24px] py-[24px] px-[8px] relative"
     >
-      <div class="absolute left-[-100px] top-[100px] rounded-full bg-primary dark:bg-white h-[14px] w-[14px]"></div>
+      <div class="absolute left-[-100px] top-[100px] rounded-full bg-primary dark:bg-primary h-[14px] w-[14px]"></div>
       <div class="px-0 sm:px-[24px] pb-[5px]">
         <p
           class="text-[2.3rem] sm:text-5xl mt-2 leading-[50px] text-center text-main font-extrabold dark:text-white"
@@ -19,16 +19,16 @@ const StepFour: Component = ({ }) => {
           Getting Things Ready for you
         </p>
         <p
-          class="mx-auto mt-2 sm:mt-6  
+          class="mx-auto mt-2 sm:mt-6 mb-6
       p-3 rounded bg-primary text-center text-sm leading-4 sm:leading-8 text-white dark:text-white shadow-md"
         >
           Don't want to wait? No problem. We will share your credentials via email once your app is ready!
         </p>
       </div>
       <ProgressBar currentStep={currentStep} />
-      <div class="max-w-[511px] mx-auto mt-2">
+      <div class="max-w-[511px] mx-auto mt-4">
 
-        <div class="py-[24px] px-[10px] sm:p-[24px] border-[#E7EBFF] rounded-[15px] border-solid shadow dark:bg-white">
+        <div class="py-[24px] px-[10px] sm:p-[24px] rounded-[15px]">
           <div class="mb-6">
             <Completed />
           </div>
